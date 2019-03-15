5dedd1ebddc8dcee515743dc1aaa23d06f25614b
Ricky-Balboa <ricardo.m.leal@hotmail.com>
2019-03-15 09:41:59 +0000

    Primeiro commit

---
+++ IncDec/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 2;
+            int c = a++ + b++;
+            int d = 70;
+            int e = 39;
+            int f = ++d + ++e;
+           
+        }
+    }
+}
