f704d86dc414289ed3ed7b528e28bb62bb825bab
Ricky-Balboa <ricardo.m.leal@hotmail.com>
2019-03-15 09:54:16 +0000

    Resolução de Exercicios

--- IncDec/Program.cs
+++ IncDec/Program.cs
@@ -12,6 +12,13 @@ namespace IncDec
             int d = 70;
             int e = 39;
             int f = ++d + ++e;
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("a++ + b++ = " + c);
+            Console.WriteLine("d = " + d);
+            Console.WriteLine("e = " + e);
+            Console.WriteLine("++d + ++e = " + f);
+            Console.ReadKey();
            
         }
     }
