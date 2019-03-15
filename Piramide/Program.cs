96f6fd0dc4da4564625b80e5d2e530cb92617db4
Ricky-Balboa <ricardo.m.leal@hotmail.com>
2019-03-15 12:27:29 +0000

    Resolução do exercicio 4

--- Piramide/Program.cs
+++ Piramide/Program.cs
@@ -12,13 +12,13 @@ namespace Piramide
             int n = Convert.ToInt32(num);
             int i, spaces;
 
-            for (i = 0; i <= n; i++)
+            for (i = 1; i <= n; i++)//executar enquanto i é menor que numero de andares
             {
-                for (spaces = 1; spaces <= n - i; spaces++)
+                for (spaces = 1; spaces <= n - i; spaces++)//executar enquanto numero de espaçoe é menor ou igual a andares menos numero da "ronda"
                     Console.Write(" ");
-                for (spaces = 1; spaces <= 2 * i - 1; spaces++)
+                for (spaces = 1; spaces <= 2* i - 1; spaces++)//executar enquanto o numero de espaços é menor ou igual a 2*i -1
                     Console.Write("*");
-                Console.Write("\n");
+                    Console.Write("\n");
             }
             Console.ReadKey();
             
---
+++ PrecorreString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PrecorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Defina a frase que deseja trabalhar");
+            string s = Console.ReadLine();
+            Console.WriteLine("Escolha uma Letra para omitir");
+            string s2 = Console.ReadLine();
+            char C = Convert.ToChar(s2);
+
+
+            foreach (char c in s)
+            {
+                if (C != c)
+                    Console.Write(c);
+                else
+                    Console.Write("");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
